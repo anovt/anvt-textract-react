8420365ec820d2c9b54e0af53b069ca014cd756c
anoopvt <armia247>
2023-05-25 22:42:18 +0530

    edited
    Signed-off-by: anoopvt <armia247>

---
+++ src/component/DiagnosticsList.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const DiagnosticsList = (props) => {
+    //console.log(props.diagnostics, "props.diagnostics");
+  return (
+    
+    <div>
+      <h2>Diagnostics Report</h2>
+      <ul>
+      {props.diagnostics.map((item,i) => (
+          <li key={i}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DiagnosticsList;

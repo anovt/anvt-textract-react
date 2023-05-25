1125b3ef6d8e1d49a954c8e9e03e636ad03567bf
anoopvt <armia247>
2023-05-25 22:42:09 +0530

    edited
    Signed-off-by: anoopvt <armia247>

--- src/component/TextAnalyze.js
+++ src/component/TextAnalyze.js
@@ -1,7 +1,11 @@
 import { Link, useParams } from "react-router-dom";
 import axios from 'axios';
+import { useState } from "react";
+import DiagnosticsList from "./DiagnosticsList";
 const TextAnalyze = () => {
   const { jobsId } = useParams();
+
+   const [diagnostics, setDiagnostics] = useState([]);
 
   const textAnazlyze = async () => {
 
@@ -9,7 +13,7 @@ const TextAnalyze = () => {
         try {
           const res = await axios.post("http://localhost:4000/textanalyze", data);
           console.log(res.data);
-
+          setDiagnostics(res.data.parsedData['diagnostics']);
           if(res.data.status === 'error')
           {
             alert(res.data.message);
@@ -22,6 +26,7 @@ const TextAnalyze = () => {
     <div>
       Analysing Text
       <button onClick={textAnazlyze}>Get Text Analyze Data</button>
+      <DiagnosticsList diagnostics = {diagnostics}/>
     </div>
   );
 };
